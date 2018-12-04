f07e9851f4d2261a231305ca93f043c822a52348
Rick Zheng <rzheng@presidio.com>
2018-12-04 13:11:35 -0600

    adding filter function to the script

--- snaplyzer/snaplyzer.py
+++ snaplyzer/snaplyzer.py
@@ -5,11 +5,21 @@ session = boto3.Session(profile_name='snaplyzer')
 ec2 = session.resource('ec2')
 
 @click.command()
-def list_instances():
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
     # dock strings.
     "List EC2 instances"
-    for i in ec2.instances.all():
-        print(','.join((
+    instances = []
+
+    if project:
+        filters = [{'Name': 'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        print(', '.join((
             # the attribue name is from the boto3 documentation https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#instance 
             i.id,
             i.instance_type,
@@ -18,9 +28,6 @@ def list_instances():
             i.public_dns_name)))
 
     return
-
-
 # as best practice to see if there's an imported function 
 if __name__ == '__main__':
     list_instances()
-#new
